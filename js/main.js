4daa1b3477355f516a683ee096aab726fac0d9cd
jtsingh1 <97473286+jtsingh1@users.noreply.github.com>
2023-06-04 01:53:50 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/*********************************************************************************
+*  WEB422 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: JasTej Student ID: 130244213 Date: june 3 2023
+*
+********************************************************************************/ 
 // Variables
 let tripData = [];
 let currentTrip = {};
